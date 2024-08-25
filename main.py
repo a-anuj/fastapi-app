c5eff941bef336502913ce91130e0908fde0b286
Anuj A <anujamirthalingam5@gmail.com>
2024-08-26 00:15:07 +0530

    Added a view specific post function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,17 +12,31 @@ class Post(BaseModel):
     content : str
     rating : Optional[int] = None
 
-post_arr = [{"id":"1","title":"CSK","content":"5 trophies"},{"id":"2","title":"KKR","content":"3 trophies"}]
+post_arr = [{"id":1,"title":"CSK","content":"5 trophies"},{"id":2,"title":"KKR","content":"3 trophies"}]
+
+def get_correct_post(id):
+    for post in post_arr:
+        if post['id'] == id:
+            return post
 
 @app.get("/")
 def root():
     return {"message":"Hello World"}
 
 @app.get("/posts")
-def posts():
+def view_posts():
     return {"data":post_arr}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post:Post):
-    post.dict()
+    post_dict = post.dict()
+    post_dict["id"] = randrange(1,1000000)
+    post_arr.append(post_dict)
+    return {"data":post_dict}
+
+@app.get("/posts/{id}")
+def view_post(id: int):
+    post = get_correct_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Path not found")
     return {"data":post}

26b9097e053d22cc5035ce64de2222243066df5d
Anuj A <anujamirthalingam5@gmail.com>
2024-08-25 22:55:36 +0530

    Created a HTTP post request and executed using postman

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
+#Github Personal Access Token - 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
 app = FastAPI()
 
 @app.get("/")
-async def root():
+def root():
     return {"message":"Hello World"}
+
+@app.get("/posts")
+def posts():
+    return {"post":"Welcome to my page!"}
+
+@app.post("/create/post")
+def create_post(payload: dict=Body(...)):
+    return {"title":payload['title'],
+            "content":payload['content']
+            }

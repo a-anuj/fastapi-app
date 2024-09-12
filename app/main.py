b1f24ea3d1b04ccb9e6998f64612680c5902c14d
Anuj A <anujamirthalingam5@gmail.com>
2024-09-12 21:59:55 +0530

    Modified all the path operations by linking them with database using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from multiprocessing.sharedctypes import synchronized
+
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 import psycopg2
@@ -54,35 +56,53 @@ def view_posts(db: Session = Depends(get_db)):
     return {"data":posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post:Post):
-    cursor.execute("""INSERT INTO posts(title,content,published) VALUES(%s,%s,%s) RETURNING * """,
-                   (post.title,post.content,post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post:Post, db: Session = Depends(get_db)):
+    #cursor.execute("""INSERT INTO posts(title,content,published) VALUES(%s,%s,%s) RETURNING * """,
+    #               (post.title,post.content,post.published))
+    #new_post = cursor.fetchone()
+    #conn.commit()
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data":new_post}
 
 @app.get("/posts/{id}")
-def view_post(id: int):
-    cursor.execute("""SELECT * FROM posts where id = %s""",(id,))
-    post = cursor.fetchone()
+def view_post(id: int,db: Session = Depends(get_db) ):
+    #cursor.execute("""SELECT * FROM posts where id = %s""",(id,))
+    #post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Path not found")
     return {"data":post}
 
 @app.delete("/posts/{id}")
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts where id=%s returning *""",(id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post is None:
+def delete_post(id: int,db: Session = Depends(get_db) ):
+    #cursor.execute("""DELETE FROM posts where id=%s returning *""",(id,))
+    #deleted_post = cursor.fetchone()
+    #conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post:Post):
-    cursor.execute("""UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s returning *""",(post.title,post.content,post.published,id))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post is None:
+def update_post(id: int, updated_post:Post,db: Session = Depends(get_db)):
+    #cursor.execute("""UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s returning *""",(post.title,post.content,post.published,id))
+    #updated_post = cursor.fetchone()
+    #conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
-    return {"data":updated_post}
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return {"data":post_query.first()}

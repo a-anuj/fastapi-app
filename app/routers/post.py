b8e5ff8e9ad34edb17cb6ae47c6c4314e47d2272
Anuj A <anujamirthalingam5@gmail.com>
2024-09-18 18:07:46 +0530

    Created separate folder for routes for the app

---
+++ app/routers/post.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import models,schemas
+from ..database import get_db
+from typing import List
+
+router = APIRouter()
+
+@router.get("/posts", response_model=List[schemas.PostResponse])
+def view_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_post(post:schemas.PostCreate, db: Session = Depends(get_db)):
+    #cursor.execute("""INSERT INTO posts(title,content,published) VALUES(%s,%s,%s) RETURNING * """,
+    #               (post.title,post.content,post.published))
+    #new_post = cursor.fetchone()
+    #conn.commit()
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@router.get("/posts/{id}", response_model=schemas.PostResponse)
+def view_post(id: int,db: Session = Depends(get_db) ):
+    #cursor.execute("""SELECT * FROM posts where id = %s""",(id,))
+    #post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Path not found")
+    return post
+
+@router.delete("/posts/{id}")
+def delete_post(id: int,db: Session = Depends(get_db) ):
+    #cursor.execute("""DELETE FROM posts where id=%s returning *""",(id,))
+    #deleted_post = cursor.fetchone()
+    #conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put("/posts/{id}", response_model=schemas.PostResponse)
+def update_post(id: int, updated_post:schemas.PostCreate,db: Session = Depends(get_db)):
+    #cursor.execute("""UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s returning *""",(post.title,post.content,post.published,id))
+    #updated_post = cursor.fetchone()
+    #conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    post = post_query.first()
+
+    if post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return post_query.first()

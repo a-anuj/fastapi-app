f8893f5cafc5d4f6d95f9b6ef7a1d28bc9fdda80
Anuj A <anujamirthalingam5@gmail.com>
2024-09-24 16:57:09 +0530

    Return owner_id of the post

--- app/schemas.py
+++ app/schemas.py
@@ -12,7 +12,8 @@ class PostCreate(PostBase):
 
 class PostResponse(PostBase):
     id:int
-    created_at : datetime
+    created_at: datetime
+    owner_id: int
 
     class Config:
         orm_mode = True

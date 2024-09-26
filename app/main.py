f82141eb230018ad2b7fc255199fae7cea7d4d5c
Anuj A <anujamirthalingam5@gmail.com>
2024-09-26 07:38:03 +0530

    Clean up main file

--- app/main.py
+++ app/main.py
@@ -1,42 +1,10 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-
-from sqlalchemy.orm import Session
-from sqlalchemy.util import deprecated
-from . import models,schemas, utils, oauth2
-from .database import engine, get_db
-from typing import List
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import user, post, auth
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost",database="fastapi",user="postgres",
-                                password="anuj2006",cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        break
-    except Exception as error:
-        print("Connection to database failed!")
-        print("Error : ",error)
-        time.sleep(2)
-
-
-post_arr = [{"id":1,"title":"CSK","content":"5 trophies"},{"id":2,"title":"KKR","content":"3 trophies"}]
-
-def get_correct_post(id):
-    for post in post_arr:
-        if post['id'] == id:
-            return post
-
-def get_post_index(id):
-    for index,post in enumerate(post_arr):
-        if post['id'] == id:
-            return index
 
 app.include_router(post.router)
 app.include_router(user.router)

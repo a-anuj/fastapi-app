f9497d7f576ad1cb3aad3cf4649e24f5496f88c2
Anuj A <anujamirthalingam5@gmail.com>
2024-10-01 17:38:39 +0530

    return number of votes for every post

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,17 +3,17 @@ from sqlalchemy.orm import Session
 from .. import models,schemas, oauth2
 from ..database import get_db
 from typing import List
+from sqlalchemy import func
 
 router = APIRouter(
     prefix = "/posts",
     tags = ['Post']
 )
 
-@router.get("/", response_model=List[schemas.PostResponse])
+@router.get("/", response_model=List[schemas.PostOut])
 def view_posts(db: Session = Depends(get_db),current_user:int = Depends(oauth2.get_current_user),
                limit:int = 10,skip:int = 0,search:str = ""):
-
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    posts = (db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id==models.Post.id,isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all())
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
@@ -28,11 +28,10 @@ def create_post(post:schemas.PostCreate, db: Session = Depends(get_db), current_
     db.refresh(new_post)
     return new_post
 
-@router.get("/{id}", response_model=schemas.PostResponse)
+@router.get("/{id}", response_model=schemas.PostOut)
 def view_post(id: int,db: Session = Depends(get_db),current_user:int = Depends(oauth2.get_current_user) ):
-    #cursor.execute("""SELECT * FROM posts where id = %s""",(id,))
-    #post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+    post = db.query(models.Post,func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id==models.Post.id,isouter=True).group_by(models.Post.id).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Path not found")
     return post

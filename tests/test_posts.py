a123b122141077b2500fa16a7a976ed8661aa2f4
Anuj A <anujamirthalingam5@gmail.com>
2024-10-06 18:30:09 +0530

    testing unauthorized users

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -3,5 +3,14 @@ from app import schemas
 
 def test_get_all_posts(authorized_client,test_posts):
     response = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.PostOut(**post)
+    post_map = map(validate,response.json())
+    post_list = list(post_map)
     assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
+
+def test_unauthorized_user_get_all_posts(client,test_posts):
+    response = client.get("/posts/")
+    assert response.status_code == 401
